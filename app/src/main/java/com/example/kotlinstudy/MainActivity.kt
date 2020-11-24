d0dcc39d030b4c38fd981a17259f58bdae4a9d6e
trad-fh <gsj990916@hanyang.ac.kr>
2020-11-25 00:24:39 +0900

    7주차

---
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val a : String = "안녕하세여여여"
+    }
+}
